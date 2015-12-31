ca3e62878c1905ddbfadc8aeb161f7985b0af0b9
AlissonMedeiros <alisson.medeiros@gmail.com>
2015-12-31 13:10:32 -0200

    AR-3049 - Code Review

--- business/src/main/java/com/softexpert/business/FeatureEnablingService.java
+++ business/src/main/java/com/softexpert/business/FeatureEnablingService.java
@@ -1,5 +1,7 @@
 package com.softexpert.business;
 
+import static com.softexpert.persistence.QFeature.feature;
+
 import javax.inject.Inject;
 
 import com.softexpert.business.exception.FeatureEnablingException;
@@ -10,12 +12,19 @@ public class FeatureEnablingService {
 
 	@Inject
 	private FeatureRepository repository;
-	
-	public boolean toggle(Long id, Boolean state) throws FeatureEnablingException{
-		QFeature feature = QFeature.feature;
-		if( repository.toogleState(state, feature.id.eq(id).and(feature.enabled.eq(!state))) > 0)
+
+	public boolean toggle(Long id, Boolean state) throws FeatureEnablingException {
+		if (updateState(id, state))
 			return state;
 		throw new FeatureEnablingException("Teste já está desabilito/habilitado");
+	}
+
+	private boolean updateState(Long id, Boolean state) {
+		return executeUpdate(id, state) > 0;
+	}
+
+	private long executeUpdate(Long id, Boolean state) {
+		return repository.toogleState(state, feature.id.eq(id).and(feature.enabled.eq(!state)));
 	}
 
 }
