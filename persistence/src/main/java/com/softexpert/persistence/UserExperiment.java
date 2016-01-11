f629e00e5857f02f26bf34e6cb68d040c605f465
AlissonMedeiros <alisson.medeiros@gmail.com>
2016-01-11 14:23:34 -0200

    Remoção de import

--- persistence/src/main/java/com/softexpert/persistence/UserExperiment.java
+++ persistence/src/main/java/com/softexpert/persistence/UserExperiment.java
@@ -1,6 +1,5 @@
 package com.softexpert.persistence;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
